81b03efc54b7128e0b4fbd4a74a0f477490687f2
Basgolfer <Basgolfer@gmail.com>
2017-10-18 19:07:26 -0400

    lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println("Would you like to multiply (type m) or sum (type s)");
+        scanner.nextLine();
+        String response = scanner.nextLine();
+        if ("m".equalsIgnoreCase(response)) {
+            System.out.println("The multiplited sum of 1 to " + number + " is " + multiply(number));
+        }
+        else if ("s".equalsIgnoreCase(response)) {
+            System.out.println("The sum of 1 to " + number + " is " + add(number));
+        }
 
+
+    }
+
+    public static int multiply(int number) {
+        int sum = 1;
+        for (int i = 1; i <= number; i++) {
+            sum = sum * i;
+        }
+        return sum;
+    }
+
+    public static int add(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = sum + i;
+        }
+        return sum;
     }
 }
