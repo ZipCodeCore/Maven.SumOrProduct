d6e72b2c8c1ae693747b64eacd7c57ddab8404e7
graham <graham@zipcoders-MacBook-Pro.local>
2017-10-19 11:59:38 -0400

    Sum or Product Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,46 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
 
-    }
+        int number;
+        int numSum = 0;
+        long numProd = 1;
+        String choice = new String("");
+
+        System.out.println("Enter a number:");
+
+        Scanner inputNum = new Scanner(System.in);
+        number = inputNum.nextInt();
+
+        System.out.println("Sum or Product? ");
+
+        Scanner inputSumProd = new Scanner(System.in);
+        choice = inputSumProd.nextLine();
+
+        if("sum".equals(choice)) {
+
+            for (int i = 0; i <= number; i++) {
+                numSum += i;
+            }
+
+            System.out.println(numSum);
+
+        }
+
+        else if ("product".equals(choice)){
+
+            for (int i = 1; i <= number; i++) {
+                numProd *= i;
+
+            }
+
+            System.out.println(numProd);
+
+        }
+
+
+
+}
 }
