393599343e8a079e253bcef9d9c196c3024f878b
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-10-19 20:29:40 -0400

    JLawrence-SumOfProduct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +6,31 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner newScan = new Scanner(System.in);
+        Scanner newScan2 = new Scanner(System.in);
+
+        System.out.println("Enter 1 to multiply enter 2: ");
+
+        int usrChoice = newScan2.nextInt();
+
+
+        int numToCalc = 0;
+
+        if (usrChoice == 1){
+            System.out.print("Please enter a number to add: ");
+            int usrNumber = newScan.nextInt();
+           for(int x = usrNumber; x > 0; --x  )
+            numToCalc +=x;
+        } else {
+            numToCalc = numToCalc + 1;
+            System.out.print("Please enter a number to multiply: ");
+            int usrNumber = newScan.nextInt();
+           for(int x = usrNumber; x > 0; --x  )
+               numToCalc *=x;
+        }
+
+       System.out.print("Your total is: " + numToCalc );
+        }
+
+
     }
-}
