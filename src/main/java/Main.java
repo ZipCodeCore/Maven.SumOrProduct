90575a4d3088a18194a33b768a068cd4746a9862
senorcl0ud <claudemcalpin@gmail.com>
2017-10-19 21:23:02 -0400

    I learned that too many variables can get confusing as hell reall quick.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,50 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        System.out.println("Give me another number.");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+// Tried to declare all my variables in one spot. Not sure if good idea will ask about later.
+
+        int result = 0;
+
+
+// Not sure if missing something above will have to check back.
+
+
+
+        System.out.println(" Okay time to switch it up. Do you want me to multiply or add?");
+
+        String wantProductOrSum = scanner.next();
+        if ("multiply".equalsIgnoreCase(wantProductOrSum)){
+            result++;
+            for (int i = 1; i <= number; i++) {
+                result = i * result;
+
+                System.out.println(i);
+            }
+
+
+
+        } else if ("add".equalsIgnoreCase(wantProductOrSum)){
+            for (int i = 1; i <= number; i++) {
+                result = i + result;
+
+                System.out.println(i);
+            }
+
+
+        } else {
+            System.out.println("go away");
+        }
+            System.out.println(result);
     }
 }
