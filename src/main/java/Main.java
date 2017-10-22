aa7fa1cf6c3be1581fd147b48c04dbd649e3dd5f
andrea <andrea@zipcoders-MacBook-Pro-2.local>
2017-10-22 12:57:32 -0400

    finished sumofproduct

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+        //test
+        //testtest
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int userNumber = scanner.nextInt();
+        int product = 1;
 
-    public static void main(String[] args){
-
+        for (int i = 1; i <= userNumber; i++) {
+            product *= userNumber;
+            System.out.println(product);
+        }
     }
 }
