d26ffff8c2e0cda6baf94e5b066f1baae38f609a
Patrick Glavin <patrickglavin@zipcoders-MacBook-Pro.local>
2018-02-07 08:44:23 -0500

    finalizing form, all functions working properly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,20 +13,24 @@ public class Main {
         String choice = reader.next();
         //now that we have number and choice, write an if or switch statement
         int i;
+        int j;
         int feederAnswer = 0;
+        int feederProduct = 1;
         if (choice.equals("sum")){
             for (i = number; i > 0; i--){
                 feederAnswer += i;
-                System.out.println(feederAnswer);
+                //System.out.println(feederAnswer);
             }
+            System.out.println("Your sum is " + feederAnswer);
         } else if (choice.equals("product")){
-            for (i = number; i > 0; i--){
-                feederAnswer = feederAnswer * i;
-                System.out.println(feederAnswer);
+            for (j = number; j > 0; j--){
+                //System.out.println(j);
+                feederProduct = feederProduct * j;
+                //System.out.println(feederProduct);
             }
+            System.out.println("Your product is " + feederProduct);
         } else {
             System.out.println("Please select sum or product.");
         }
-        System.out.println(feederAnswer);
     }
 }
