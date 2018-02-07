5f15b1801d336cc2e2081e2034e0760024428bf9
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-07 11:51:29 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,32 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
-public class Main {
+import java.util.*;
 
+public class Main {
     public static void main(String[] args){
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a whole number: ");
+        int input = scan.nextInt();
+
+        Scanner scan2 = new Scanner(System.in);
+
+        System.out.println("Sum or Product?");
+        String op = scan2.nextLine();
+
+        int sum = 0;
+        int product = 1;
+
+        if(op.equals("Sum")) {
+            for (int i = 1; i <= input; i++) {
+                sum += i;
+            }
+            System.out.println(sum);
+        } else if(op.equals("Product")) {
+            for(int i = 1; i <= input; i ++) {
+                product *= i;
+            }
+            System.out.println(product);
+        }
     }
 }
+
