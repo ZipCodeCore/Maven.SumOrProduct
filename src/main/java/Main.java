417dc5316a4fc13be633ea515807a2ac1a3ba090
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-02-07 15:57:40 -0500

    SumOrProduct Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,40 @@ public class Main {
 
     public static void main(String[] args){
 
+        Scanner number = new Scanner(System.in);
+        System.out.println("Enter a number");
+        String answer = number.nextLine();
+        int givennumber = Integer.parseInt(answer);
+        System.out.println(answer);
+
+        Scanner chooseoption = new Scanner(System.in);
+        System.out.println("would you like to add or multiply?");
+        String addOrMultiply = chooseoption.nextLine();
+
+        if (addOrMultiply.equalsIgnoreCase("add")) {
+            int sum = 0;
+            for (int i = 1; i <= givennumber; i++) {
+                sum = sum + i;
+
+            }
+            System.out.println(sum);
+        }
+        else if (addOrMultiply.equalsIgnoreCase("multiply")){
+            int product = 1;
+            for (int i = 1; i <= givennumber; i++) {
+                product = product * i;
+
+            }
+            System.out.println(product);
+    }
+
+
     }
 }
+
+
+
+
+
+
+
