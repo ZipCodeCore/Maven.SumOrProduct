1738f38399ad3787f56f62d96148ed3d03dd30bf
Patrick Glavin <patrickglavin@zipcoders-MacBook-Pro.local>
2018-02-06 22:58:02 -0500

    first third done, need to write switch statement next

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your Number");
+        int number = reader.nextInt();
+        System.out.println("Sum or product?");
+        String choice = reader.next();
+        //now that we have number and choice, write an if or switch statement
     }
 }
