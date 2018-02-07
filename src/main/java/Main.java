e36c57693fa5132549ce90b4e6f8198769281910
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-02-07 00:12:20 -0500

    sum or product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+
+        Scanner scanNum = new Scanner(System.in);
+        Scanner scanString = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+
+        int userNumber = scanNum.nextInt();
+
+        System.out.println("Would you like to multiply or add " + userNumber + " to 1.... please enter multiply or add: ");
+
+        String userString = scanString.nextLine();
+
+        if (userString.equals("add")) {
+            System.out.println("Your total is: " + (userNumber + 1));
+        } else if (userString.equals("multiply")) {
+            System.out.println("Your total is: " + (userNumber * 1));
+        }
 
     }
 }
