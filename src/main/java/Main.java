d02e88324dc4acf120fee19c421fe3fda63d1692
Karousha Fennimore <karoushafennimore@zipcoders-MacBook-Pro.local>
2018-02-07 06:42:15 -0500

    Almost solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,45 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
 
-    }
+        System.out.println("Please enter a number: ");
+        // 3. Use the Scanner to read a line of text from the user.
+        String userInput = input.nextLine();
+
+        System.out.println("Would you like to add or multiply? ");
+        // 3. Use the Scanner to read a line of text from the user.
+        String computeInput = input.nextLine();
+
+        if(computeInput.equalsIgnoreCase("add")) {
+
+            int numInput = Integer.parseInt(userInput);
+            int sum = 0;
+
+            for (int i = 1; i <= numInput; i++) {
+
+                sum += i;
+             }
+            System.out.println(sum);
+
+        } else if (computeInput.equalsIgnoreCase("multiply")) {
+
+            int numInput = Integer.parseInt(userInput);
+            int sum = 1;
+
+            for (int i = 1; i <= numInput; i++) {
+
+                sum *= i;
+            }
+            System.out.println(sum);
+
+        }
+
+
+}
 }
