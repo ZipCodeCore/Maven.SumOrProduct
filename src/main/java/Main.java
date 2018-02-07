08ac12fd56d04640f9068641e8fc2fbd83660b54
kfennimore <32980838+kfennimore@users.noreply.github.com>
2018-02-07 14:01:34 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,17 +9,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.println("Please enter a number: ");
-<<<<<<< HEAD
         int personNum = input.nextInt();
 
         System.out.println("Would you like to add or multiply? ");
         String computeInput = input.next();
-=======
-        String userInput = input.nextLine();
-
-        System.out.println("Would you like to add or multiply? ");
-        String computeInput = input.nextLine();
->>>>>>> f4f28d38976360eb699a92cc8d69d0c2bfefcb0f
 
         if(computeInput.equalsIgnoreCase("add")) {
             int sum = 0;
@@ -28,11 +21,11 @@ public class Main {
                 sum += i;
              }
              System.out.println(sum);
+
         } else if (computeInput.equalsIgnoreCase("multiply")) {
             int multTotal = 1;
 
             for (int i = 1; i <= personNum; i++) {
-
                 multTotal *= i;
             }
             System.out.println(multTotal);
