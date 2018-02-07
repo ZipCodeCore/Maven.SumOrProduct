f753f8db181ca8ee57ea672ef67c66f123d2a1bd
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-02-07 17:39:18 -0500

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner userNumb = new Scanner(System.in);
+        System.out.println("enter an integer");
+        int number = userNumb.nextInt();
+        Scanner aom = new Scanner(System.in);
+        System.out.println("Would you like to add or multiply?");
+        String addOrMult = aom.nextLine();
+        int result = 0;
+        if(addOrMult.equalsIgnoreCase("add")) {
+            for(int i = 0; i<=number; i++) {
+                result += i;
+            }
+        }
+        if(addOrMult.equalsIgnoreCase("multiply")) {
+            result = 1;
+            for(int i = number; i>0; i--) {
+                result *= i;
+            }
+            System.out.println("Your answer is " + result);
+
+        }
 
     }
 }
