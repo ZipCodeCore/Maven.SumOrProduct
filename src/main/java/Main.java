7985d116fb4acfe3baa4a0a66db42860c1b87c76
Patrick Glavin <patrickglavin@zipcoders-MacBook-Pro.local>
2018-02-07 08:29:44 -0500

    sum working, need to fix product

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,5 +12,21 @@ public class Main {
         System.out.println("Sum or product?");
         String choice = reader.next();
         //now that we have number and choice, write an if or switch statement
+        int i;
+        int feederAnswer = 0;
+        if (choice.equals("sum")){
+            for (i = number; i > 0; i--){
+                feederAnswer += i;
+                System.out.println(feederAnswer);
+            }
+        } else if (choice.equals("product")){
+            for (i = number; i > 0; i--){
+                feederAnswer = feederAnswer * i;
+                System.out.println(feederAnswer);
+            }
+        } else {
+            System.out.println("Please select sum or product.");
+        }
+        System.out.println(feederAnswer);
     }
 }
