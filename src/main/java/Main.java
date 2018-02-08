a88bd4aa17a816e3de27b4df2a31f4d61db9074d
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-3.local>
2018-02-08 18:02:16 -0500

    Thursday

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,58 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Do you Want Sum or Product?");
+
+        String name = in.nextLine();
+
+        System.out.println("What Number Would You Like?");
+
+
+        if ("Sum".equalsIgnoreCase(name)){
+
+            int test = in.nextInt();
+
+            int result = 0;
+
+            for (int i = 0; i <= test; i++)
+
+                result += i;
+
+            System.out.println(result);
+        }
+
+
+
+        else if ("Product".equalsIgnoreCase(name)) {
+
+            int test = in.nextInt();
+
+            int result = 1;
+
+
+            for (int i = 1; i <= test; i++)
+
+                result *= i;
+
+            System.out.println(result);
+        }
+
+
+
+
 
     }
+
 }
