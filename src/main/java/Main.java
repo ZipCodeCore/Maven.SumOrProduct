11d658107f3dee98c3580ddc366e323b7c2e5613
Dylan Camac <dylancamac@zipcoders-MacBook-Pro-5.local>
2018-02-08 09:05:30 -0500

    saved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,27 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+    import java.util.Scanner;
+
+
 public class Main {
 
     // comment comment
 
+
+
     public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+    System.out.println("Input a number");
+    int input = scan.nextInt();
+    System.out.println("Would you like the sum or product of " + input + "?");
+    String preference = scan.nextLine();
+
+    if (preference.equalsIgnoreCase("sum")) {
+
+    }
 
     }
 }
