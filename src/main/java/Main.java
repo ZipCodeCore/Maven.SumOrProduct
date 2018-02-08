2c41ae15994b7b15b4828f84821703c84f494eeb
Sean Mis <seanmis@MacBook-Pro-3.local>
2018-02-07 19:28:44 -0500

    First Try??

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
+        Scanner inputNum = new Scanner(System.in);
+        System.out.println("Please input a number.");
+        int userInput = inputNum.nextInt();
+        System.out.println(userInput);
+        Scanner userChoice = new Scanner(System.in);
+        System.out.println("Would you like to add or multiply your number?");
+        String userAnswer = userChoice.nextLine();
+        int answer = 0;
 
+        if (userAnswer.equals("add")) {
+            for(int i=1; i <= userInput; i++) {
+                answer+=i;
+            }
+        } else if (userAnswer.equals("multiply")) {
+            for(int i=1; i <= userInput; i++) {
+                answer*=i;
+            }
+        }
+        System.out.println(answer);
     }
 }
