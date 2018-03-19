b0d7dc13b9e75402088ece031f535bd4e21e2d07
Dominique Clarke <dominiquedclarke@gmail.com>
2018-03-19 13:35:07 -0400

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,98 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        promptUser(true);
+    }
 
+    public static void promptUser(boolean firstPrompt) {
+        Scanner input = new Scanner(System.in);
+        int i = 1;
+        int result = 0;
+
+        int n = getNumber(input, firstPrompt);
+        String operator = getOperator(input, firstPrompt);
+
+        if (operator.equals("add"))
+        {
+            while (i <= n)
+            {
+                result += i;
+                i++;
+            }
+        }
+
+        if (operator.equals("multiply"))
+        {
+            result = 1;
+
+            while (i <= n)
+            {
+                result *= i;
+                i++;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static boolean isInteger(String s) {
+        try
+        {
+            Integer.parseInt(s);
+            return true;
+        }
+        catch (NumberFormatException ex)
+        {
+            return false;
+        }
+    }
+
+    public static int getNumber(Scanner input, boolean firstPrompt) {
+        int n = 0;
+        if (firstPrompt)
+        {
+            System.out.println("Enter an integer.");
+        }
+        else {
+            System.out.println("Please enter an integer.");
+        }
+
+        String s = input.nextLine();
+
+        if (isInteger(s))
+        {
+            n = Integer.parseInt(s);
+            return n;
+        }
+        else
+        {
+            return getNumber(input, false);
+        }
+
+    }
+
+    public static String getOperator(Scanner input, boolean firstPrompt) {
+        if (firstPrompt)
+        {
+            System.out.println("Would you like to add or multiply?");
+        }
+        else
+        {
+            System.out.println("Please choose add or multiply.");
+        }
+
+        String s = input.nextLine();
+
+        if(!s.equals("add") && !s.equals("multiply"))
+        {
+            return getOperator(input, false);
+        }
+
+        return s;
     }
 }
